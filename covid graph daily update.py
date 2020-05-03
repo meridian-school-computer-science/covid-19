8a43928a903a2c4c4a008e5c83c843966d57e98e
kevmac64 <k_mac87@live.com>
2020-05-03 06:19:28 -0500

    Update 3 May

--- covid graph daily update.py
+++ covid graph daily update.py
@@ -8,8 +8,8 @@ import csv
 from matplotlib import pyplot as plt
 import datetime as dt
 path = 'C:\\Users\\k_mac\\OneDrive\\Meridian\\h Computer Science\\Teacher Projects\\COVID\\data\\global\\'
-filename = 'total_cases_2_may.csv'
-date_title = '2 May'
+filename = 'total_cases_3_may.csv'
+date_title = '3 May'
 
 # a change also from inside github
 
--- covid us states graph.py
+++ covid us states graph.py
@@ -15,7 +15,7 @@ import datetime as dt
 
 path = 'C:\\Users\\k_mac\\OneDrive\\Meridian\\h Computer Science\\Teacher Projects\\COVID\\data\\us\\'
 filename = 'mac_us_states_deaths.csv'
-date_title = '2 May'
+date_title = '3 May'
 death_rate_adjustment = 250
 
 states = ['California', 'Texas', 'Florida', 'New York', 'Pennsylvania', 'Illinois', 'Ohio', 'Georgia', 'North Carolina',
@@ -66,11 +66,12 @@ us_transpose['PA NY+9'] = death_rate_adjustment * us_transpose['Pennsylvania'].s
 
 # set the title
 plt.title('States COVID-19 Estimated Cases')
-plt.title('Log Scale', loc='left')
+#plt.title('Log Scale', loc='left')
 plt.title(date_title, loc='right')
+plt.title('Linear', loc='left')
 
 ax = plt.gca()  # get current axis
-plt.yscale('log')
+#plt.yscale('log')
 for i, state in enumerate(states_of_interest): 
     us_transpose.plot(kind='line', x='Days from Today', y=state, color=colors[i], ax=ax)
 
