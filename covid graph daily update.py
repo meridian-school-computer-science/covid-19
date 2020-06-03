678397657676f956d3e830c470c977fea3a3093d
kevmac64 <k_mac87@live.com>
2020-06-03 05:19:28 -0500

    Update 3 Jun

--- covid graph daily update.py
+++ covid graph daily update.py
@@ -8,8 +8,8 @@ import csv
 from matplotlib import pyplot as plt
 import datetime as dt
 path = 'C:\\Users\\k_mac\\OneDrive\\Meridian\\h Computer Science\\Teacher Projects\\COVID\\data\\global\\'
-filename = 'total_cases_2_jun.csv'
-date_title = '2 Jun'
+filename = 'total_cases_3_jun.csv'
+date_title = '3 Jun'
 
 # a change also from inside github
 
