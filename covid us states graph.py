eae6fc302607fce1039ea19304a6b9213e4fd637
kevmac64 <k_mac87@live.com>
2020-07-19 08:18:51 -0500

    Update 19 July

--- covid us states graph.py
+++ covid us states graph.py
@@ -15,7 +15,7 @@ import datetime as dt
 
 path = 'C:\\Users\\k_mac\\OneDrive\\Meridian\\h Computer Science\\Teacher Projects\\COVID\\data\\us\\'
 filename = 'mac_us_states_deaths.csv'
-date_title = '18 Jul'
+date_title = '19 Jul'
 death_rate_adjustment = 250
 
 states = ['California', 'Texas', 'Florida', 'New York', 'Pennsylvania', 'Illinois', 'Ohio', 'Georgia', 'North Carolina',
