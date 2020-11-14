be87ec2a8b6890f0a109a1f713437d6325877e88
kevmac64 <k_mac87@live.com>
2020-11-14 12:52:49 -0600

    Update 14 Nov

--- covid graph daily update.py
+++ covid graph daily update.py
@@ -12,8 +12,8 @@ import csv
 from matplotlib import pyplot as plt
 import datetime as dt
 path = 'C:\\Users\\k_mac\\OneDrive\\Meridian\\h Computer Science\\Teacher Projects\\COVID\\data\\global\\'
-filename = 'total_cases_13_nov.csv'
-date_title = '13 Nov'
+filename = 'total_cases_14_nov.csv'
+date_title = '14 Nov'
 
 """ DATA SOURCE:
 Hasell, J., Mathieu, E., Beltekian, D. et al. A cross-country database of COVID-19 testing. Sci Data 7, 345 (2020)
